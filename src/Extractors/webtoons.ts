3797cdd1b361a18506ed7d14ed005a086966fec7
Long Nguyen <nguyen.long.908132@gmail.com>
2022-05-02 19:27:06 +0700

    [webtoons] Remove unused import

--- src/Extractors/webtoons.ts
+++ src/Extractors/webtoons.ts
@@ -1,6 +1,5 @@
 import { Info, MangaInfo, ChapterInfo } from "../Info.js";
 import { Extractor } from "../Extractor.js";
-import { range } from "../Utils/Range.js";
 
 export default class webtoons extends Extractor
 {
