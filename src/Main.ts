75cdae99bd497917527043a3cddfa71567147884
Long Nguyen <nguyen.long.908132@gmail.com>
2022-05-11 23:41:28 +0700

    [Main] Extra logging

--- src/Main.ts
+++ src/Main.ts
@@ -3,17 +3,21 @@ import { settings } from "./Settings.js";
 import { loadExtractors } from "./Extractor.js";
 import { Downloader } from "./Downloader.js";
 import { resolve } from "path";
+import { Logger } from "./Logger.js";
 
 await loadExtractors();
 
-console.log(settings);
-
 prompt.start();
 
-console.log("Please input the link and output folder:");
+const logger = new Logger("Main");
+logger.log("Current settings:", settings);
+logger.log("Please input the link and output folder:");
 let {link, output} = <{link: string, output: string}>await prompt.get(["link", "output"]);
 
 if (output === "")
+{
     output = resolve("./output");
+    logger.log(`Using default output folder: ${output}`);
+}
 
 await Downloader.download(link, output);
